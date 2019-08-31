5e11d3686c36fc9e4c279c3c1de4141ad9873e1e
navegador <navegador@030_01_05_72737>
2019-08-30 22:31:18 -0300

    Componente Safari.Framework: logging y serviceFactory

---
+++ Shared/Safari.Framework/Logging/ILoggingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Safari.Framework.Logging
+{
+    public partial interface ILoggingService
+    {
+        void Log(string message);
+        void Error(string message);
+        void Error(Exception ex);
+        void Initialise(int maxLogSize);
+        IList<LogEntry> ListLogFile();
+        void Recycle();
+        void ClearLogFiles();
+    }
+}
